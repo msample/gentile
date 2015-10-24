e82df10cb263d6022a3d0e6794ad0b195f2b34e1
Mike Sample <msample@users.noreply.github.com>
2015-10-24 00:30:53 -0700

    Initial checkin

---
+++ README.md
@@ -0,0 +1,70 @@
+QUICKSTART
+==========
+
+*gentile.py* is a quick hack that will convert a jpeg of a map into
+tiles that Garmin GPS unit can display instead of the regular
+basemap.  
+
+run it like this:
+
+    gentile.py -t 30 -i North-Shore_49.470808_49.336874_-122.980824_-123.131780.jpg   
+
+and it will split it into 1024x1024x pixel tile jpegs, each wrapped in a kmz file:
+
+    North-Shore_000.kmz	North-Shore_007.kmz	North-Shore_014.kmz	North-Shore_021.kmz	North-Shore_028.kmz
+    North-Shore_001.kmz	North-Shore_008.kmz	North-Shore_015.kmz	North-Shore_022.kmz	North-Shore_029.kmz
+    North-Shore_002.kmz	North-Shore_009.kmz	North-Shore_016.kmz	North-Shore_023.kmz	North-Shore_030.kmz
+    North-Shore_003.kmz	North-Shore_010.kmz	North-Shore_017.kmz	North-Shore_024.kmz	North-Shore_031.kmz
+    North-Shore_004.kmz	North-Shore_011.kmz	North-Shore_018.kmz	North-Shore_025.kmz	North-Shore_032.kmz
+    North-Shore_005.kmz	North-Shore_012.kmz	North-Shore_019.kmz	North-Shore_026.kmz	North-Shore_033.kmz
+    North-Shore_006.kmz	North-Shore_013.kmz	North-Shore_020.kmz	North-Shore_027.kmz	North-Shore_034.kmz
+
+Plug in your GPS via USB and copy the kmz files to its /Garmin/CustomMaps/ directory.  These maps tiles should now be 
+displayed on you GPS when you look at that area.
+
+SYSTEM REQUIREMENTS
+===================
+
+* python 2.7
+* imagemagick
+* zip
+
+Tested on OS X 10.10 using brew to install imagemagick.  
+
+MAP FILE NAMES
+==============
+
+Maps can be jpeg, tiff or other formats supported by ImageMagick. The
+file name of your map must include its bounding box (lat/long) in
+decimal degrees. The syntax is:
+  
+    North-Shore_49.470808_49.336874_-122.980824_-123.131780.jpg   
+
+    <name>_<north-lattitude>_<south_lattitude>_<east-longitude>_<west_longitude>
+
+Don't use underscore anywhere in the name except as one of the four separators above. 
+
+
+FIGURING OUT THE BOUNDING BOX
+=============================
+
+Try using GoogleEarth (Add->Image Overlay) and fiddling with the
+coordinates in the Location tab to postion the map.  Then try those
+coordinates with gentile.py.  Tune it by using the GPS with the kmz
+files and observe if it's offset N-S and/or E-W.  Regenerate the tiles
+with adjusted coordinates.
+
+HOW MANY TILES
+==============
+
+The '-t' parameter suggests the maximum number of tiles to
+generate. Setting too low may make for grainy tile images because
+gentile.py will reduce the resolution of the image until it can be cut
+it into ~maxtiles 1024x1024 pieces.
+
+Setting maxtiles too high may exceed the capacity of your GPS device.  A
+Garmin 62s can hold a maxiumum of 100 tiles while some other models support 500
+
+Run the following for other options:
+  
+   gentile.py -h
---
+++ gentile.py
@@ -0,0 +1,191 @@
+#!/usr/bin/env python
+#
+# Creates .kmz map tiles for a Garmin from a larger geo-poisitioned map image. Tested on a 62s
+# This crunches and converts an input image (.jpg,.gif,.tiff etc) to match what Garmin devices expect
+#
+# run 'gentile.py -h' to get the usage.
+#
+# Connect your GPS via USB and copy the generated kmz files into /Garmin/CustomMap (SD or main mem)
+#
+# Input image: geo-positioned (lat/long bounding box) in filename. See usage. 
+#
+# Garmin limitations on .kmz files and images in them:
+#  * image must be jpeg, not 'progressive'
+#  * only considers the /doc.kml in the .kmz
+#  * tiles over 1MP, e.g. > 1024x1024 pixels, 512x2048 pixels do not add increased resolution
+#  * each tile jpeg should be less than 3MB.
+#  * Max images/tiles per device: typically 100.  500 on some.
+#  * smaller image files are rendered faster
+#
+# requires the following binaries on your system:
+#   convert (part of imagemagick)
+#   identify (part of imagemagick)
+#   zip
+
+import argparse 
+import re
+import sys
+import subprocess
+import os
+import tempfile
+from string import Template
+
+kmlHeader = '''<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2" xmlns:kml="http://www.opengis.net/kml/2.2" xmlns:atom="http://www.w3.org/2005/Atom">
+'''
+
+kmlOverlayTemplate = '''<GroundOverlay>
+  <name>$name</name>
+  <color>bdffffff</color>
+  <drawOrder>$drawingOrder</drawOrder>
+  <Icon>
+    <href>$tileFileName</href>
+    <viewBoundScale>1.0</viewBoundScale>
+  </Icon>
+  <LatLonBox>
+    <north>$north</north>
+    <south>$south</south>
+    <east>$east</east>
+    <west>$west</west>
+    <rotation>0.0</rotation>
+  </LatLonBox>
+</GroundOverlay>
+'''
+
+kmlFooter ='''</kml>
+'''
+
+
+def parseArgs():
+    '''Returns (args,parser). parser in case you want to dump help'''
+    parser = argparse.ArgumentParser(description="Convert a map image to a bunch of map tiles (.kmz files) for your Garmin handheld GPS")
+
+    parser.add_argument('--image', '-i',
+                        help='an image file named with its bounding box in decimal degrees. Underscores are required: <map-name>_<North-lat>_<South-lat>_<East-long>_<West-long>.* E.g. Grouse-Mountain_49.336694_49.470628_49.470628_-123.132056_-122.9811.jpg')
+
+    parser.add_argument('--maxTiles', '-t', type=int, default=100,
+                        help='Garmin limits the max tiles per model (100 on most, 500 on Montana, Oregon 600 series and GPSMAP 64 series. Tiles of more than 1 megapixel (w*h) add no additional clarity. If you have a large image, it will be reduced in quality until it can be chopped in max-tiles or less 1024x1024 chunks. Defaults to 100.')
+
+    parser.add_argument('--drawingOrder', '-d', type=int, default=51,
+                        help='values greater than 50 mean the Garmin device puts this map on top. If you have multiple overlapping maps, set the top one with the highest drawing-order. Defaults to 51.')
+
+    args = parser.parse_args()
+
+    if args.image is None:
+        parser.print_help()
+        sys.exit(-1)
+
+    return (args,parser)
+
+
+
+def getMapAttrsFromArgs(args, parser):
+    '''Returns tuple (mapNameStr, northLatFloat, southLatFloat, eastLongFloat, westLongFloat)'''
+    m = re.search("([^/_]+)_([^_]+)_([^_]+)_([^_]+)_([^_]+)\..*",  args.image)
+    name = m.group(1)
+    north = m.group(2)
+    south = m.group(3)
+    east = m.group(4)
+    west = m.group(5)
+    decDegreesRe = '-?[0-9]+\.[0-9]+'
+    if  not (re.match(decDegreesRe, north) and re.match(decDegreesRe, south) and 
+             re.match(decDegreesRe, east) and re.match(decDegreesRe, west)):
+        parser.error(args.image + " - malformed image file name. Decimal degrees of bounding box required in file name")
+        sys.exit(-1)
+
+    return (name,float(north),float(south),float(east),float(west))
+
+
+def getImageWxH(imageFilename):
+    '''Returns an int two-tuple (width,height) of the given image file in pixels'''
+    whList = subprocess.check_output(['identify', '-format', '%w %h', imageFilename]).decode('utf-8').split()
+    return (int(whList[0]),int(whList[1]))
+
+
+def rmrf(startDir):
+    '''Danger! equiv of: rm -rf startDir'''
+    for (dir,childDirnames,filenames) in os.walk(startDir, topdown=False):
+        for f in filenames:
+            os.remove(os.path.join(dir,f))
+        for d in childDirnames:
+            os.rmdir(os.path.join(dir,d))
+    os.rmdir(startDir)
+
+
+def main():
+    (args,parser) = parseArgs()
+    imageFilename = os.path.abspath(args.image)
+    (name,north,south,east,west) = getMapAttrsFromArgs(args,parser)
+    (width,height) = getImageWxH(imageFilename)
+
+    # create temp dir and cd there. zip needs working dir so skip working with abs paths
+    wdir = tempfile.mkdtemp(prefix='gentile-', dir='.')
+    #tilesdir = wdir + '/tiles'
+    tilesdir = os.path.join(wdir,'tiles')
+    os.makedirs(tilesdir)
+    os.chdir(wdir)
+    tilesdir = 'tiles'
+
+    maxPixels = args.maxTiles * 1024 * 1024
+    if (maxPixels < (height * width)):
+        newImageFilename = 'tmp1.jpg'
+        subprocess.check_output(['convert', imageFilename, '-resize', '@'+str(maxPixels), newImageFilename])
+        imageFilename = newImageFilename
+
+    newImageFilename = 'tmp2.jpg'
+    subprocess.check_output(['convert', imageFilename, '-strip', '-interlace', 'none', newImageFilename ])
+    imageFileName = newImageFilename
+
+    # create 1024x1024 tiles from the image in tiledir
+    subprocess.check_output(['convert', '-crop', '1024x1024', imageFilename, os.path.join(tilesdir, name + '_tile_%03d.jpg')])
+
+    (fullWidth,fullHeight) = getImageWxH(imageFilename)
+
+    template = Template(kmlOverlayTemplate)
+    tileNorth = north
+    tileWest = west
+    tileWidthSum = 0
+    dirCount = 0
+
+    # stash each tile in its own kmz file
+    for tileFile in sorted(os.listdir('tiles')):
+        # right edge or bottom may not be 1024x1024
+        (tileWidth,tileHeight) = getImageWxH(os.path.join(tilesdir,tileFile))
+        nsDelta = (float(tileHeight)/fullHeight)*(north-south)
+        ewDelta = (float(tileWidth)/fullWidth)*((east-west)%360)
+        tileWidthSum += tileWidth
+        tileEast = tileWest + ewDelta
+        tileSouth = tileNorth - nsDelta
+        tileName = name + ('_%03d' % dirCount)
+        dirCount += 1
+        kdir = tileName
+        os.makedirs(kdir)
+        kmlFile = open(os.path.join(kdir,'doc.kml'), 'w+')
+        kmlFile.write(kmlHeader)
+        overlay = template.substitute(north=tileNorth, south=tileSouth, east=tileEast, west=tileWest, name=tileFile, drawingOrder=args.drawingOrder, tileFileName=tileFile)
+        kmlFile.write(overlay)
+        kmlFile.write(kmlFooter)
+        kmlFile.close()
+
+        os.rename(os.path.join(tilesdir, tileFile), os.path.join(kdir,tileFile))
+        os.chdir(kdir)
+        subprocess.check_output(['zip', '-r', '../' + tileName + '.kmz', '.'])
+        os.chdir('..')
+        if (tileWidthSum >= fullWidth):
+            tileNorth = tileSouth
+            tileWest = west
+            tileWidthSum = 0
+        else:
+            tileWest = tileEast
+
+    # move all the kmz files to original working dir and nuke this tmp-dir
+    files = os.listdir('.')
+    for f in files:
+        if f.endswith('.kmz'):
+            os.rename(f, '../' + f)
+    os.chdir('..')
+    rmrf(wdir)
+
+
+if __name__ == "__main__":
+    main()
